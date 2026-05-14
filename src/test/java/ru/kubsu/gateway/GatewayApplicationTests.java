b4215d25d411eb13ff7fa689d17a4dbb6f89ebcb
dkhlybov <khdfkrr@gmail.com>
2026-05-14 19:46:11 +0300

    Create gateway

--- src/test/java/ru/kubsu/gateway/GatewayApplicationTests.java
+++ src/test/java/ru/kubsu/gateway/GatewayApplicationTests.java
@@ -1,13 +1,36 @@
 package ru.kubsu.gateway;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class GatewayApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    void setUp() {
+        webTestClient = WebTestClient.bindToServer()
+                .baseUrl("http://127.0.0.1:" + port)
+                .build();
+    }
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void smokeRouteForwardsToHealth() {
+        webTestClient.get().uri("/api/smoke")
+                .exchange()
+                .expectStatus().isOk();
+    }
 }
